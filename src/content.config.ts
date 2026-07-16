115b48ac8e7b57e4f76ff69d55baafbbdc5734fc
netgoo <jacky13689@outlook.com>
2026-07-16 10:53:21 +0800

    fix: 改用astro content集合读取文章，解决新增mdoc线上不展示缓存问题

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,10 @@
 ﻿import { defineCollection, z } from 'astro:content';
-
-const posts = defineCollection({
+const blog = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),
-    description: z.string().optional(),
-  }),
+    description: z.string().optional()
+  }).passthrough()
 });
-
-export const collections = { posts };
+export const collections = { blog };
