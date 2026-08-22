e45521e00e4cc30ca62e9553f1e2267b3a7db9b4
netgoo <jacky13689@outlook.com>
2026-08-22 12:20:02 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// ALEX AUTOMATION 核心配置文件
+// ALEX AUTOMATION 10年总站无漏洞终极静态配置文件
 export default defineConfig({
-  // 1. 主域名
   site: 'https://wenboom.com',
   base: '/',
   trailingSlash: 'ignore',
-  output: 'static', // 
+  output: 'static', // 扣死纯静态输出
+  adapter: null,    // 显式彻底卸载一切遗留云函数渲染适配器，断绝 500 报错根源
 });
+
