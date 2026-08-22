f41acb80e630a0d72781c4991ecc0aedb238440f
netgoo <jacky13689@outlook.com>
2026-08-22 10:44:16 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import markdoc from '@astrojs/markdoc';
-import sitemap from '@astrojs/sitemap';
-import robotsTxt from 'astro-robots-txt';
-import keystatic from '@keystatic/astro';
-import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel';
-
+// ALEX AUTOMATION 核心配置文件
 export default defineConfig({
-  site: 'https://stellarshepherd.com',
-  output: 'server',
-  integrations: [mdx(), markdoc(), sitemap(), robotsTxt(), react(), keystatic({ config: "./keystatic.config.ts" })],
-
-  adapter: vercel(),
+  // 1. 主域名
+  site: 'https://wenboom.com',
+  base: '/',
+  trailingSlash: 'ignore',
+  output: 'static', // 
 });
