9c8a50f89c6b4aa42ba0d15ffa43e35614528eac
netgoo <jacky13689@outlook.com>
2026-08-26 01:53:00 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://wenboom.com',
-  base: '/',
-  trailingSlash: 'ignore',
-  output: 'static', // Astro 7 中保持 static 即可，接口文件设置 prerender = false 会自动生效
-  adapter: vercel(),
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
