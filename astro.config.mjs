ac60a5b2fb9489a416571588d4def50a8c3e7a55
netgoo <jacky13689@outlook.com>
2026-08-26 02:03:40 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://www.wenboom.com',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
